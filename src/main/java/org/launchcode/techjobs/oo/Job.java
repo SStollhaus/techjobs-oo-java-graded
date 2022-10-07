4465ff93006b2765e43a9f1a8456045ce0920671
Sean Stollhaus <seanstollhaus@Seans-MBP.local>
2022-10-06 19:14:18 -0500

    All Test Passing

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -74,31 +74,58 @@ public class Job {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
+        if (!(o instanceof Job)) return false;
         Job job = (Job) o;
-
-        if (id != job.id) return false;
-        if (!Objects.equals(name, job.name)) return false;
-        if (!Objects.equals(employer, job.employer)) return false;
-        if (!Objects.equals(location, job.location)) return false;
-        if (!Objects.equals(positionType, job.positionType)) return false;
-        return Objects.equals(coreCompetency, job.coreCompetency);
+        return getId() == job.getId();
     }
 
     @Override
     public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (employer != null ? employer.hashCode() : 0);
-        result = 31 * result + (location != null ? location.hashCode() : 0);
-        result = 31 * result + (positionType != null ? positionType.hashCode() : 0);
-        result = 31 * result + (coreCompetency != null ? coreCompetency.hashCode() : 0);
-        return result;
+        return Objects.hash(getId());
     }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        Job job = (Job) o;
+//
+//        if (id != job.id) return false;
+//        if (!Objects.equals(name, job.name)) return false;
+//        if (!Objects.equals(employer, job.employer)) return false;
+//        if (!Objects.equals(location, job.location)) return false;
+//        if (!Objects.equals(positionType, job.positionType)) return false;
+//        return Objects.equals(coreCompetency, job.coreCompetency);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int result = id;
+//        result = 31 * result + (name != null ? name.hashCode() : 0);
+//        result = 31 * result + (employer != null ? employer.hashCode() : 0);
+//        result = 31 * result + (location != null ? location.hashCode() : 0);
+//        result = 31 * result + (positionType != null ? positionType.hashCode() : 0);
+//        result = 31 * result + (coreCompetency != null ? coreCompetency.hashCode() : 0);
+//        return result;
+//    }
 
     @Override
     public String toString() {
+        if (name.equals("")) {
+            setName("Data not available");
+        }
+        if (employer.getValue().equals("")) {
+            employer.setValue("Data not available");
+        }
+        if (location.getValue().equals("")) {
+            location.setValue("Data not available");
+        }
+        if (positionType.getValue().equals("")) {
+            positionType.setValue("Data not available");
+        }
+        if (coreCompetency.getValue().equals("")) {
+            coreCompetency.setValue("Data not available");
+        }
         return ("\n" + "ID: " + id + "\nName: " + name + "\nEmployer: " + employer + "\nLocation: " + location + "\nPosition Type: " + positionType + "\nCore Competency: " + coreCompetency + "\n");
     }
 // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
